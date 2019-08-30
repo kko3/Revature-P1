e7a6829dc82bd3f09bcdc4b5773216b205c89c4e
kko3 <kko3@zips.uakron.edu>
2019-08-30 10:37:21 -0500

    added id to ipizzaparts and those who inherit from it

--- PizzaBox.Domain/Interfaces/IPizzaParts.cs
+++ PizzaBox.Domain/Interfaces/IPizzaParts.cs
@@ -2,6 +2,7 @@ namespace PizzaBox.Domain.Interfaces
 {
     public interface IPizzaParts
     {
+        public int Id { get; set; }
         string Name { get; set; }
         decimal Price { get; set; }
     }
--- PizzaBox.Domain/Models/Crust.cs
+++ PizzaBox.Domain/Models/Crust.cs
@@ -4,6 +4,7 @@ namespace PizzaBox.Domain.Models
 {
   public class Crust : IPizzaParts
   {
+    public int Id { get; set; }
     public string Name { get; set; }
     public decimal Price { get; set; }
 
