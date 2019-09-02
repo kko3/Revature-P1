181788a5671a5adc5daadf8e90fb514a40999851
kko3 <kko3@zips.uakron.edu>
2019-09-01 23:04:58 -0500

    will create a pizza in pizzacontroller

--- PizzaBox.Client/Controllers/PizzaController.cs
+++ PizzaBox.Client/Controllers/PizzaController.cs
@@ -1,30 +1,42 @@
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
+using PizzaBox.Data;
 using PizzaBox.Domain.Models;
 
 namespace  Pizzabox.Client.Controllers
 {
     public class PizzaController : Controller
     {
+        private PizzaBoxDBContext _db = new PizzaBoxDBContext();
+
         [HttpGet]
         public IActionResult CreatePizza()
         {
           return View();
         }
         [HttpPost]
-        public IActionResult CreatePizza(Crust c,Size s, List<Topping> t)
+        public IActionResult CreatePizza(int crust,int size, List<int> topping)
         {
+          Crust c = _db.Crusts.Find(crust);
+          Size s = _db.Sizes.Find(size);
+
+          decimal cost = 0;
           decimal toppingcost = 0;
+
+          List<Topping> t = new List<Topping>();
+          foreach (var item in topping)
+          {
+              t.Add(_db.Toppings.Find(item));
+          }
           foreach (var item in t)
           {
               toppingcost += item.Price;
           }
+         
+         cost = c.Price + s.Price + toppingcost;
+         Pizza p = new Pizza(c,s,t,cost);
 
-          decimal cost = c.Price + s.Price+toppingcost;
-           Pizza p = new Pizza(c,s,t,cost);
-
-           //return RedirectToAction("ViewPizza",p);
-           return View();
+          return View("ViewPizza",p);
         }
 
         public IActionResult ViewPizza(Pizza p)
