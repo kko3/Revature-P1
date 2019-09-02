869e3a808671f761d732ba19a1da095eb52af08f
kko3 <kko3@zips.uakron.edu>
2019-09-01 21:42:57 -0500

    createpizza and viewpizza set up but not working

--- PizzaBox.Client/Controllers/PizzaController.cs
+++ PizzaBox.Client/Controllers/PizzaController.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using PizzaBox.Domain.Models;
 
@@ -5,10 +6,31 @@ namespace  Pizzabox.Client.Controllers
 {
     public class PizzaController : Controller
     {
-        public IActionResult CreatePizza(Crust c,Size s, Topping t)
+        [HttpGet]
+        public IActionResult CreatePizza()
         {
-           
+          return View();
+        }
+        [HttpPost]
+        public IActionResult CreatePizza(Crust c,Size s, List<Topping> t)
+        {
+          decimal toppingcost = 0;
+          foreach (var item in t)
+          {
+              toppingcost += item.Price;
+          }
+
+          decimal cost = c.Price + s.Price+toppingcost;
+           Pizza p = new Pizza(c,s,t,cost);
+
+           //return RedirectToAction("ViewPizza",p);
            return View();
+        }
+
+        public IActionResult ViewPizza(Pizza p)
+        {
+
+          return View(p);
         }
     }
 }
